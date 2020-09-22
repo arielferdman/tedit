6d85cc71e95f2265e4168203de9941f8f0be37f9
arielferdman <feariel@gmail.com>
2020-09-22 06:07:55 +0300

    initial commit

---
+++ tedit-state-chart.ts
@@ -0,0 +1,39 @@
+
+class State {
+  editor: HTMLDivElement;
+  suggester: HTMLDivElement;
+  constructor() {
+    this.editor = document.querySelector('#editor');
+    this.suggester = document.querySelector('#suggester')
+  }
+}
+
+class MetaState extends State {
+  constructor() {
+    super();
+  }
+}
+
+class EditState extends State {
+    constructor() {
+      super();
+    }
+
+    printchr(chr: string): boolean {
+      this.editor.innerHTML += chr;
+      return true;
+    }
+
+    focusEditor(): boolean {
+      this.editor.focus();
+      return true;
+    }
+}
+
+class EditorStateChart {
+  state: State;
+
+  constructor() {
+    this.state = new EditState();
+  }
+}
